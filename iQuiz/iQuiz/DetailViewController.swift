10f5690a250d3c0eb780d81b868569cfb0217bcb
Alex N. Schenck <alex379@ougl-20-d.eplt.washington.edu>
2015-11-10 00:53:33 -0800

    Full quiz functionality. Only remaining feature is back button

--- iQuiz/iQuiz/DetailViewController.swift
+++ iQuiz/iQuiz/DetailViewController.swift
@@ -17,13 +17,28 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var CorrectAnswerIndicator: UILabel!
     @IBOutlet weak var ContinueButton: UIButton!
     @IBOutlet weak var FinishButton: UIButton!
+    @IBOutlet weak var ResultsLabel: UILabel!
     
     @IBOutlet var QuestionView: UIView!
     @IBOutlet var AnswerView: UIView!
     @IBOutlet var FinishView: UIView!
     
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return count(chosenQuiz.questions[questionNumber].answers)
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return chosenQuiz.questions[questionNumber].answers[row]
+    }
+    
     var chosenQuiz : MasterViewController.Quiz  = MasterViewController.Quiz(title: "", description: "", imagePath: "", questions: [])
     var questionNumber : Int = 0
+    var chosenAnswer : Int = 0
+    var numberCorrect : Int = 0
     
     var detailItem: AnyObject? {
         didSet {
@@ -43,27 +58,62 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
         
-        // Find out what view loaded, load resulting info
-        if (QuestionView != nil) {
-            QuestionPrompt.text = chosenQuiz.questions[questionNumber].prompt
-            questionNumber++
+        // Find out what view loaded, load resulting info into view
+        // FinishView
+        if (FinishView != nil)
+        {
+            ResultsLabel.text = "You're done! You got \(numberCorrect) right out of \(count(chosenQuiz.questions))."
         }
-        else if (AnswerView != nil) {
-            if (questionNumber < count(chosenQuiz.questions)) {
-                FinishButton.hidden = true
+        else
+        {
+            var currentQuestion = chosenQuiz.questions[questionNumber]
+            
+            // QuestionView
+            if (QuestionView != nil) {
+                QuestionPrompt.text = currentQuestion.prompt
             }
-            else {
-                ContinueButton.hidden = true
+            // AnswerView
+            else if (AnswerView != nil) {
+                CorrectAnswer.text = currentQuestion.answers[currentQuestion.correctAnswer]
+                
+                if (chosenAnswer == currentQuestion.correctAnswer)
+                {
+                    CorrectAnswerIndicator.text = "You were right!"
+                    numberCorrect++
+                }
+                else
+                {
+                    CorrectAnswerIndicator.text = "Your answer was wrong :("
+                }
+                
+                questionNumber++
+                
+                if (questionNumber < count(chosenQuiz.questions)) {
+                    FinishButton.hidden = true
+                }
+                else {
+                    ContinueButton.hidden = true
+                }
             }
-        }
-        else if (FinishView != nil) {
         }
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        let controller = segue.destinationViewController as! DetailViewController
-        controller.chosenQuiz = chosenQuiz
-        controller.questionNumber = questionNumber
+        // Set new chosen answer if it was QuestionView
+        if (QuestionView != nil)
+        {
+            chosenAnswer = QuestionAnswerPicker.selectedRowInComponent(0)
+        }
+        
+        // Go to next detailView if current view is not FinishView
+        if (FinishView == nil)
+        {
+            let controller = segue.destinationViewController as! DetailViewController
+            controller.chosenQuiz = chosenQuiz
+            controller.questionNumber = questionNumber
+            controller.chosenAnswer = chosenAnswer
+            controller.numberCorrect = numberCorrect
+        }
     }
 
     override func didReceiveMemoryWarning() {
